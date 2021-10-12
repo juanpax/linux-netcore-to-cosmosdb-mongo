effecdf942ce5f86ad1975437c723717468c44d5
Juan Pablo <36493244+juanpax@users.noreply.github.com>
2021-10-11 22:48:54 -0600

    Create Employee.cs

---
+++ Employee.cs
@@ -0,0 +1,10 @@
+	namespace app
+	{
+	    public class Employee
+	    {
+	        public string id { get; set; }
+	        public string name { get; set; }
+	        public string age { get; set; }
+	        public string gender { get; set; }
+	    }
+}
